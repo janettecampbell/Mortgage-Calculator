bd3f126c4c9df2d2ff2ed5da27916775a346fdc5
Janette Campbell <campbjp@hotmail.com>
2022-10-03 11:24:49 -0400

    chore: changed spacing to make more readable

--- src/com/janettecampbell/Main.java
+++ src/com/janettecampbell/Main.java
@@ -1,8 +1,33 @@
 package com.janettecampbell;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principle: ");
+        float principle = Float.parseFloat(scanner.nextLine());
+
+        System.out.print("Annual Interest Rate: ");
+        float annualRate = Float.parseFloat(scanner.nextLine());
+
+        System.out.print("Period (Years): ");
+        float years = scanner.nextFloat();
+
+        float monthlyRate = (annualRate / PERCENT) / MONTHS_IN_YEAR;
+        float payments = years * MONTHS_IN_YEAR;
+        double mortgage = principle * ((monthlyRate * (Math.pow(1 + monthlyRate,payments))) / (Math.pow(1 + monthlyRate, payments) - 1));
+
+        System.out.println(currency.format(mortgage));
+
+
+
     }
 }
